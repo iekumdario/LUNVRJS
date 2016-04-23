ffa57e414ebe6c42118029c7894ab38812d9d449
LightLex <lexxxiss@gmail.com>
2016-04-23 18:37:55 -0500

    Rotation and light

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 var canvas = document.getElementById("renderCanvas");
         var engine = new BABYLON.Engine(canvas, true);
-
+        var x = 0;
+        var z = 0;
+        var y = 0;
+        var phase;
+        var sphere;
         var createScene = function () {
         
             // This creates a basic Babylon Scene object (non-mesh)
             var scene = new BABYLON.Scene(engine);
         
             // This creates and positions a free camera (non-mesh)
-            var camera = new BABYLON.VRDeviceOrientationFreeCamera ("Camera", new BABYLON.Vector3 (0, 0, -15), scene, 0);
+            var camera = new BABYLON.VRDeviceOrientationFreeCamera("Camera", new BABYLON.Vector3 (0, 5, -20), scene, 0);
         
             // This targets the camera to scene origin
             camera.setTarget(BABYLON.Vector3.Zero());
@@ -16,22 +20,22 @@ var canvas = document.getElementById("renderCanvas");
             camera.attachControl(canvas, true);
         
             // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(-5, 5, -2), scene);
+            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(30, 0, -25), scene);
         
             // Default intensity is 1. Let's dim the light a small amount
             light.intensity = 0.7;
         
             // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-            var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 15, scene);
+            sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 15, scene);
         
             // Move the sphere upward 1/2 its height
             sphere.position.y = 1;
+            sphere.position.z = 10;
             var materialSphere1 = new BABYLON.StandardMaterial("texture1", scene);
             //materialSphere1.diffuseColor = new BABYLON.Color3(0, 1, 0.35);
             materialSphere1.diffuseTexture = new BABYLON.Texture("js/texture/moon.jpg", scene);
             materialSphere1.bumpTexture = new BABYLON.Texture("js/texture/moonbump.png", scene);
             materialSphere1.specularPower = 100;
-			//materialSphere1.emissiveColor = new BABYLON.Color3(0.18, 0.60, .89);
 
             sphere.material = materialSphere1;
             var skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
@@ -46,6 +50,49 @@ var canvas = document.getElementById("renderCanvas");
             skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("js/texture/"+currentSky+"/skybox", scene);
             skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
 
+            try{
+                var test = Android.getMoonPhase();
+                phase = JSON.parse(test);
+                Android.printPosition(phase.phaseName);
+            }catch(e){
+                phase = {phase:1};
+            }
+
+                    switch (phase.phase) {
+            case 0:
+                light.direction = new BABYLON.Vector3(0, 0, 0);
+                light.intensity = 0.1;
+                break;
+            case 1:
+                light.direction = new BABYLON.Vector3(30, 0, 30);
+                light.intensity = 0.5;
+                break;
+            case 2:
+                light.direction = new BABYLON.Vector3(30, 0, 10);
+                light.intensity = 0.5;
+                break;
+            case 3:
+                light.direction = new BABYLON.Vector3(30, 0, -10);
+                light.intensity = 0.5;
+                break;
+            case 4:
+                light.direction = new BABYLON.Vector3(10, 0, -30);
+                light.intensity = 0.5;
+                break;
+            case 5:
+                light.direction = new BABYLON.Vector3(-20, 0, -20);
+                light.intensity = 0.5;
+                break;
+            case 6:
+                light.direction = new BABYLON.Vector3(-30, 0, 0);
+                light.intensity = 0.5;
+                break;
+            case 7:
+                light.direction = new BABYLON.Vector3(-30, 0, 30);
+                light.intensity = 0.5;
+                break;
+            default:break;
+        }
             return scene;
         
         };
@@ -53,6 +100,9 @@ var canvas = document.getElementById("renderCanvas");
         var scene = createScene();
 
         engine.runRenderLoop(function () {
+            x+=0.002;
+            z+=0.0020;
+            sphere.position=new BABYLON.Vector3(Math.cos(x)*25,0,Math.sin(x)*20-15);
             scene.render();
         });
 
--- js/parts/luna.js
+++
@@ -1,4 +0,0 @@
-var geometry   = new THREE.SphereGeometry(1, 32, 32);
-var material  = new THREE.MeshPhongMaterial();
-material.map    = THREE.ImageUtils.loadTexture('../texture/moon.jpg');
-var luna = new THREE.Mesh(geometry, material);
