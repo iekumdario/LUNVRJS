5ca8947b7a549836355d7bd447acf62557806ebc
Light <lexxxiss@gmail.com>
2016-04-23 12:33:33 -0500

    Mayor changes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,47 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var canvas = document.getElementById("renderCanvas");
+        var engine = new BABYLON.Engine(canvas, true);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+        var createScene = function () {
+        
+            // This creates a basic Babylon Scene object (non-mesh)
+            var scene = new BABYLON.Scene(engine);
+        
+            // This creates and positions a free camera (non-mesh)
+            var camera = new BABYLON.VRDeviceOrientationFreeCamera ("Camera", new BABYLON.Vector3 (-6.7, 1.2, -1.3), scene, 0);
+        
+            // This targets the camera to scene origin
+            camera.setTarget(BABYLON.Vector3.Zero());
+        
+            // This attaches the camera to the canvas
+            camera.attachControl(canvas, true);
+        
+            // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+        
+            // Default intensity is 1. Let's dim the light a small amount
+            light.intensity = 0.7;
+        
+            // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+            var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+        
+            // Move the sphere upward 1/2 its height
+            sphere.position.y = 1;
+            var materialSphere1 = new BABYLON.StandardMaterial("texture1", scene);
+            //materialSphere1.diffuseColor = new BABYLON.Color3(0, 1, 0.35);
+            materialSphere1.diffuseTexture = new BABYLON.Texture("js/texture/moon.jpg", scene);
+            sphere.material = materialSphere1;
+        
+            return scene;
+        
+        };
+        
+        var scene = createScene();
 
-scene.add( luna );
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
 
-camera.position.z = 5;
-
-var render = function () {
-	requestAnimationFrame( render );
-
-	//cube.rotation.x += 0.1;
-	//cube.rotation.y += 0.1;
-
-	renderer.render(scene, camera);
-};
-
-render();
+        // Resize
+        window.addEventListener("resize", function () {
+            engine.resize();
+        });
